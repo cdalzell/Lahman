cab5a61199bfe14ec105592995142cf9a59e1f48
Ben Dilday <ben.dilday.phd@gmail.com>
2019-03-26 21:54:17 -0400

    update year in file databse file name

--- inst/scripts/readLahman.R
+++ inst/scripts/readLahman.R
@@ -1,7 +1,7 @@
 # Script to read the Lahman data base .csv files & create Rdata files
 
 # directory where the .csv files will be created
-indir <- "D:/Dev/R/Lahman/data"
+indir <- "SET ME"
 
 # directory where the .RData files will be created
 #outdir <- paste0(indir, "RData")
@@ -10,11 +10,11 @@ outdir <- indir
 setwd(indir)
 
 # local data location
-dataFile <- "../source-data/baseballdatabank-2017.1.zip"
+dataFile <- "../source-data/baseballdatabank-2019.2.zip"
 
 # no need to download if we already have the file
 if (!file.exists(dataFile)) {
-  zipfile <- "http://seanlahman.com/files/database/baseballdatabank-2017.1.zip"
+  zipfile <- "http://seanlahman.com/files/database/baseballdatabank-2019.2.zip"
   download.file(zipfile, dataFile)
 }
 
@@ -25,7 +25,7 @@ unzip(dataFile, exdir=indir)
 #Master <- read.csv(file="Master.csv", header=TRUE, stringsAsFactors=FALSE)
 
 # set indir to the directories the csv are extracted to
-indir <- paste0(indir, "/baseballdatabank-2017.1/core")
+indir <- paste0(indir, "/baseballdatabank-2019.2/core")
 setwd(indir)
 
 (files <- list.files(path=indir, pattern="*.csv$"))
@@ -68,7 +68,7 @@ colnames(HallOfFame)[2] <- 'yearID'
 # The format in the as.Date() function is the 'informat', 
 # in SASspeak; the output format is a calendar date in 
 # yyyy-mm-dd format.
-Master <- within(Master, {
+People <- within(People, {
 #    debut = as.Date(debut, 
 #                    format = '%m/%d/%Y %H:%M:%s',
 #                    origin = '1970-01-01')
@@ -88,7 +88,7 @@ Master <- within(Master, {
 #  'named Guillermo VelC!zquez' in object 'Master'
 #  'Martmn Magdaleno Dihigo (Llanos)' in object 'Master'
 
-tools:::showNonASCII(paste0(indir, 'Master.csv'))
+tools:::showNonASCII(paste0(indir, 'People.csv'))
 
 # then, fix manually, because I don't know an R way ...
 
@@ -114,7 +114,7 @@ save(HallOfFame,          file="HallOfFame.RData")
 #save(HOFold,              file="HOFold.RData")             
 save(Managers,            file="Managers.RData")           
 save(ManagersHalf,        file="ManagersHalf.RData")       
-save(Master,              file="Master.RData")       ## Master.csv fixed for non ASCII strings   
+save(People,              file="People.RData")       ## People.csv fixed for non ASCII strings   
 save(Pitching,            file="Pitching.RData")           
 save(PitchingPost,        file="PitchingPost.RData")       
 save(Salaries,            file="Salaries.RData")           
@@ -145,7 +145,7 @@ if (FALSE) {
   promptData(HOFold,              filename="HOFold.Rd")             
   promptData(Managers,            filename="Managers.Rd")           
   promptData(ManagersHalf,        filename="ManagersHalf.Rd")       
-  promptData(Master,              filename="Master.Rd")             
+  promptData(People,              filename="People.Rd")             
   promptData(Pitching,            filename="Pitching.Rd")           
   promptData(PitchingPost,        filename="PitchingPost.Rd")       
   promptData(Salaries,            filename="Salaries.Rd")           
